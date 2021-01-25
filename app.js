026c4afb6bdffea7936ebba9e272d9c0a946f712
gulsenaktas <aaktasgulsen@gmail.com>
2021-01-26 01:01:18 +0300

    1.1 add localstorage

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
 const input= document.querySelector("#input")
-const deleteFar = document.querySelector(".far")
-const items = document.querySelectorAll("li")
 const lists = document.querySelector(".lists")
 const search = document.querySelector(".search input")
 const searchForm = document.querySelector(".search")
 
 const generateTemplate = todo => {
-
     const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
         <span>${todo}</span>
@@ -18,6 +15,7 @@ const generateTemplate = todo => {
     list.innerHTML += html
 };
 
+let todosArray=[]
 addForm.addEventListener("submit", e => {
 
     e.preventDefault();
@@ -26,41 +24,46 @@ addForm.addEventListener("submit", e => {
     if(todo.length){
         generateTemplate(todo)
         input.value= ""
-    }
+    };
 
+    todosArray.push(todo)
+    localStorage.setItem("todo",JSON.stringify(todosArray));
+      
 });
 
+if(localStorage.getItem("todo")){
+    const data = JSON.parse(localStorage.getItem("todo"))
+    data.forEach(e=>{
+        generateTemplate(e)
+        todosArray.push(e)
+    });
+};
+
 searchForm.addEventListener("submit", e => {
-
     e.preventDefault();
-})
+});
 
-
- 
-// delete tools
 lists.addEventListener("click", e => {
     if(e.target.classList.contains("far")){
     e.target.parentElement.remove()
-    }
-});
 
+    const data = JSON.parse(localStorage.getItem("todo"))
+    const newArray = data.filter(element => element != e.target.parentElement.children[0].innerHTML)
+    localStorage.setItem('todo',JSON.stringify(newArray))    
+    };
+});
 
 const filterTodos = (term) => {
     Array.from(list.children)
-        //filtrelenenler dışındakilere filtred clası ver displayi none olsun;
         .filter((todo) => !todo.textContent.toLowerCase().includes(term))
         .forEach((todo) => todo.classList.add("none"));
 
     Array.from(list.children)
-        //search.value yi silince busefer bu kod hepsini tutuyor ve tuttuklarından filtred classlarını siliyor.
-        // Bu şekilde hepsini tekrardan görmüş oluyoruz
         .filter((todo) => todo.textContent.toLowerCase().includes(term))
         .forEach((todo) => todo.classList.remove("none"));    
 };
 
-// keyup tools
 search.addEventListener("keyup", () => {
     const term = search.value.toLowerCase().trim();
     filterTodos(term);
-})
-    
+});
